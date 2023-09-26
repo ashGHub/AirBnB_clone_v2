15fdb5f1192c20d325f904426af8eced918f9be8
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-27 01:31:34 +0300

    Add task 10 solution web flask

---
+++ web_flask/9-states.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Display passed text with C prepended"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """Display passed text with Python prepended"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display n if it is an integer"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Display HTML page only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with a list of all states"""
+    states = storage.all(State)
+    state = states.get('State.' + id)
+    return render_template('9-states.html', states=states, state=state, id=id)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    # Perform teardown actions here
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
